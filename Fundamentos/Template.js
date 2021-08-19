52fd699903b0131e3c5b3a181916373fd832f0af
Thiago Lima <thiagolimatt2@gmail.com>
2021-08-19 19:48:10 -0300

    Create Template.js

---
+++ Fundamentos/Template.js
@@ -0,0 +1,7 @@
+const nome = "Thiago"
+const concatenação = "Olá, " + nome + "!"
+const template = `string text ${nome} string text`
+console.log(template)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up("cuidado")}`)
